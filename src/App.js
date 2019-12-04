e03b827465696e0aeeffce55c1a684b08f1f91fb
Diego Armando Payan Lopez <diegogmr1335@gmail.com>
2019-12-03 21:06:15 -0700

    1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import * as signalR from '@aspnet/signalr';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      connection: ''
+    }
+  }
+
+  componentDidMount() {
+    let connection = new signalR.HubConnectionBuilder().withUrl("/reportesHub").build();
+    this.setState({ connection }, () => {
+      connection.start()
+        .then(() => console.log("Conexión establecida con éxito"))
+        .catch((err) => console.log(`Error: ${err}`))
+    });
+
+    connection.on("newReport", (message) => console.log(message))
+
+  }
+
+  sendMessage = () => {
+    this.state.connection.invoke("newEmergencyReport", "Hello");
+  }
+
+  render() {
+    return (
+      <div className="App">
+        Hola
+        <button onClick={() => this.sendMessage()}>Message</button>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/setupProxy.js
@@ -0,0 +1,4 @@
+const proxy = require('http-proxy-middleware');
+module.exports = app => {
+    app.use(proxy('/reportesHub', {target: 'http://localhost:5100/',  ws: true}));
+}
